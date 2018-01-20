ee9daee9c6583d6568e001d9a08d7d404415783b
Ishahids <imran@thankyo.net>
2018-01-20 16:48:22 +0500

    added project code for the first time except images
    added project code for the first time except images

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost:8080/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8080/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/xampp/htdocs/upload/admin/');
+define('DIR_SYSTEM', 'E:/xampp/htdocs/upload/system/');
+define('DIR_IMAGE', 'E:/xampp/htdocs/upload/image/');
+define('DIR_STORAGE', 'E:/xampp/storage/');
+define('DIR_CATALOG', 'E:/xampp/htdocs/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ishahid');
+define('DB_PASSWORD', 'helloworld');
+define('DB_DATABASE', 'ishahid');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
